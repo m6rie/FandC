284952b2ed5b7fb67b23d97a9c433d0f29db3716
Marie Etong <m6rief@gmail.com>
2022-12-24 00:01:07 +0000

    website opening page, styling and animation

---
+++ website/app.js
@@ -0,0 +1,29 @@
+const indexAnimation = document.querySelector("#index-animation")
+const body = document.body
+const indexSingleDivs = document.querySelectorAll(".index-div")
+const enterDiv = document.querySelector("#enter-div")
+
+// ENTER BUTTON
+enterbtn = document.createElement("button")
+enterbtn.innerHTML = "Enter"
+enterbtn.classList.add('enterbtn')
+enterDiv.append(enterbtn)
+// redirect to new page when Enter button is clicked
+enterbtn.addEventListener("click", function(e) {
+  window.location.href = "./about-me/about-me.html";
+})
+
+// EMOJIS
+// Event listener for them to appear
+window.addEventListener("load", poppingEmoji)
+// Setting the interval of 1 seconde between each new emoji's appearance
+setInterval(poppingEmoji, 1000)
+
+function poppingEmoji() {
+  emojiArray = ['😊','💃🏾','🎉','😽','👩🏾‍🍳','👩🏾‍💻','✨','🧶','🔥','🌝','🤌🏾','🥳','🥸']
+
+  for(let i = 0; i < indexSingleDivs.length; i++) {
+    indexSingleDivs[Math.floor(Math.random() * indexSingleDivs.length + 1)].innerHTML = emojiArray[Math.floor(Math.random() * emojiArray.length)]
+    break;
+  }
+}
