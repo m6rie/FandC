aa7ff80940c5519e3fd84c4b467aa8c1c983c387
Marie Etong <m6rief@gmail.com>
2023-01-08 22:53:22 +0000

    carousel navigation with dots and new styling

--- website/funny-videos/funny-video.js
+++ website/funny-videos/funny-video.js
@@ -1,9 +1,18 @@
 const caroussel = document.getElementById("caroussel")
 const video = document.querySelector("#video")
-// const videos = document.getElementsByClassName("div-video")
-const backward = document.querySelector(".fa-chevron-left")
-const forward = document.querySelector(".fa-chevron-right")
+const backward = document.querySelector(".fa-caret-left")
+const forward = document.querySelector(".fa-caret-right")
 let videoTitle = document.querySelector("#videoname")
+const dots1 = document.querySelector(".dots-1")
+const dots2 = document.querySelector(".dots-2")
+const dots3 = document.querySelector(".dots-3")
+const dots4 = document.querySelector(".dots-4")
+const dots5 = document.querySelector(".dots-5")
+const dots6 = document.querySelector(".dots-6")
+const dots7 = document.querySelector(".dots-7")
+const dots8 = document.querySelector(".dots-8")
+const dotsArray = [dots1, dots2, dots3, dots4, dots5, dots6, dots7, dots8]
+let currentItem = 0
 
 const videos = [
   {
@@ -48,7 +57,81 @@ const videos = [
   }
 ]
 
-let currentItem = 0
+function selectingDot() {
+  dotsArray.forEach((dot, index) => {
+    if(dot.classList.contains("selecteddot")) {
+      dot.classList.remove("selecteddot")
+      dot.classList.add("unselecteddot")
+    }
+    if(index === currentItem) {
+      dot.classList.add("selecteddot")
+    }
+  })
+}
+
+function firstDot() {
+  currentItem = 0
+  item = videos[currentItem]
+  videoTitle.innerHTML = item.videoname
+  video.src = item.video
+  selectingDot()
+}
+
+function secondDot() {
+  currentItem = 1
+  item = videos[currentItem]
+  videoTitle.innerHTML = item.videoname
+  video.src = item.video
+  selectingDot()
+}
+
+function thirdDot() {
+  currentItem = 2
+  item = videos[currentItem]
+  videoTitle.innerHTML = item.videoname
+  video.src = item.video
+  selectingDot()
+}
+
+function fourthDot() {
+  currentItem = 3
+  item = videos[currentItem]
+  videoTitle.innerHTML = item.videoname
+  video.src = item.video
+  selectingDot()
+}
+
+function fifthDot() {
+  currentItem = 4
+  item = videos[currentItem]
+  videoTitle.innerHTML = item.videoname
+  video.src = item.video
+  selectingDot()
+}
+
+function sixthDot() {
+  currentItem = 5
+  item = videos[currentItem]
+  videoTitle.innerHTML = item.videoname
+  video.src = item.video
+  selectingDot()
+}
+
+function seventhDot() {
+  currentItem = 6
+  item = videos[currentItem]
+  videoTitle.innerHTML = item.videoname
+  video.src = item.video
+  selectingDot()
+}
+
+function eighthDot() {
+  currentItem = 7
+  item = videos[currentItem]
+  videoTitle.innerHTML = item.videoname
+  video.src = item.video
+  selectingDot()
+}
 
 window.addEventListener("load", function(e) {
   let item = videos[currentItem]
@@ -61,6 +144,7 @@ window.addEventListener("load", function(e) {
       item = videos[currentItem]
       videoTitle.innerHTML = item.videoname
       video.src = item.video
+      selectingDot()
     } else {
       console.log("nothing")
     }
@@ -73,8 +157,10 @@ window.addEventListener("load", function(e) {
       item = videos[currentItem]
       videoTitle.innerHTML = item.videoname
       video.src = item.video
+      selectingDot()
+
     } else {
-      console.log("neg")
+      console.log("negative")
     }
     e.preventDefault();
   })
